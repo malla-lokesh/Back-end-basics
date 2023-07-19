0a0ee15784a9e29fae68b206adbd9ec97f6e235d
malla-lokesh <121810307046@gitam.in>
2023-07-19 19:56:04 +0530

    task2-deliverables

---
+++ task2-deliverables.js
@@ -0,0 +1,12 @@
+const productOf2Nums = (a, b) => a * b;
+
+console.log(productOf2Nums(2, 5)); // outputs 10.
+
+const student = {
+    name: 'lokesh',
+    hello() {
+        console.log(this.name);
+    }
+}
+
+student.hello();
