4ecb149c0e68328be327138b2d8385c67d6f3ba4
malla-lokesh <121810307046@gitam.in>
2023-07-22 12:14:39 +0530

    outputting different content based on different navigation routes

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('server created!');
+    const url = req.url;
+
+    if (url === '/home') {
+        res.write('<html>');
+        res.write('<head><title>request/response</title></head>');
+        res.write('<body><h1>Welcome HOME</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else if (url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>request/response</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else if (url === '/node') {
+        res.write('<html>');
+        res.write('<head><title>request/response</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project.</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 })
 
 server.listen(4000);
