7877da4ce308f9d76059894ba5a77e71abd05522
malla-lokesh <121810307046@gitam.in>
2023-07-23 11:10:30 +0530

    Clean Up code Task.

--- app.js
+++ app.js
@@ -1,37 +1,8 @@
 const http = require('http');
 const fs = require('fs');
+const routeHandler = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    const body = [];
-
-    if (url === '/') {
-        fs.readFile('inputMessage.txt', (err, data) => {
-            if (err) console.log(err);
-            console.log(`data from file: ${data}`);
-            res.write('<html>');
-            res.write('<head><title>writing and reading from a file</title></head>');
-            res.write('<body>');
-            res.write(`<div>${data}</div>`);
-            res.write('<form action="/message" method="POST"><input type="text" name="message"/><button>send</button></form></body>');
-            res.write('</html>');
-            return res.end();
-        });
-    } else if (url === '/message' && method==='POST') {
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('inputMessage.txt', message);
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
-        });
-    }
-})
+console.log(routeHandler.text);
+const server = http.createServer(routeHandler.route);
 
 server.listen(4000);
---
+++ routes.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const routesHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    const body = [];
+
+    if (url === '/') {
+        fs.readFile('inputMessage.txt', (err, data) => {
+            if (err) console.log(err);
+            console.log(`data from file: ${data}`);
+            res.write('<html>');
+            res.write('<head><title>writing and reading from a file</title></head>');
+            res.write('<body>');
+            res.write(`<div>${data}</div>`);
+            res.write('<form action="/message" method="POST"><input type="text" name="message"/><button>send</button></form></body>');
+            res.write('</html>');
+            return res.end();
+        });
+    } else if (url === '/message' && method==='POST') {
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('inputMessage.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
+    }
+}
+
+module.exports = {
+    route: routesHandler,
+    text: 'random text'
+}
