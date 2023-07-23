47ed28e21d2835358816917d68a134eefff2b8e4
malla-lokesh <121810307046@gitam.in>
2023-07-23 13:12:15 +0530

    Debugging Techniques

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const routeHandler = require('./routes');
 
-console.log(routeHandler.text);
+console.log(`some random text adjacent another js file ${routeHandler.text}`);
 const server = http.createServer(routeHandler.route);
 
 server.listen(4000);
