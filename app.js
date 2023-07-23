804bea0877754e675d9884f615a95267333381c9
malla-lokesh <121810307046@gitam.in>
2023-07-23 10:24:54 +0530

    writing and reading from a file.

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
+    const body = [];
 
-    if (url === '/home') {
-        res.write('<html>');
-        res.write('<head><title>request/response</title></head>');
-        res.write('<body><h1>Welcome HOME</h1></body>');
-        res.write('</html>');
-        res.end();
-    } else if (url === '/about') {
-        res.write('<html>');
-        res.write('<head><title>request/response</title></head>');
-        res.write('<body><h1>Welcome to About Us page</h1></body>');
-        res.write('</html>');
-        res.end();
-    } else if (url === '/node') {
-        res.write('<html>');
-        res.write('<head><title>request/response</title></head>');
-        res.write('<body><h1>Welcome to my Node Js project.</h1></body>');
-        res.write('</html>');
-        res.end();
+    if (url === '/') {
+        fs.readFile('inputMessage.txt', (err, data) => {
+            if (err) console.log(err);
+            console.log(`data from file: ${data}`);
+            res.write('<html>');
+            res.write('<head><title>writing and reading from a file</title></head>');
+            res.write('<body>');
+            res.write(`<div>${data}</div>`);
+            res.write('<form action="/message" method="POST"><input type="text" name="message"/><button>send</button></form></body>');
+            res.write('</html>');
+            return res.end();
+        });
+    } else if (url === '/message' && method==='POST') {
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('inputMessage.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
     }
 })
 
